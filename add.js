32edfcbc04710c29aaef40f16890899edea29c95
ramakrishnarao679 <ramakrishnarao679@gmail.com>
2024-07-16 15:10:38 +0530

    all approaches added

--- add.js
+++ add.js
@@ -6,8 +6,28 @@ let number = [1, 2, 3, 4, 5];
 // }
 // console.log(sum);
 
-let sum=0; 
-for(let i of number){
-    sum = sum+i;
+// let sum=0; 
+// for(let i of number){
+//     sum = sum+i;
+// }
+// console.log(sum);
+
+// let sum=0;
+// for(var i in number){
+//     sum = sum + number[i];
+// }
+// console.log(sum);
+
+// let sum=0;
+// number.forEach(function(number){
+//     sum = sum +number;
+// })
+// console.log(sum);
+
+function sumOfnum(sum, number){
+    return sum+number;
 }
-console.log(sum)
+function add(){
+    console.log(number.reduce(sumOfnum));
+}
+add();
